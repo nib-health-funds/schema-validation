6cd26023a43a691f7e3f6587fa5c28aa35912576
Matthew Turner <matthew.turner@nib.com.au>
2016-02-01 16:16:43 +1100

    Accept a schema which contains all the things
    - to fix defaulting

--- test/index.js
+++ test/index.js
@@ -2,7 +2,8 @@
 const assert = require('assert');
 const validator = require('../lib/index.js');
 const validate = require('@nib/validation-methods');
-const defaulter = require('../lib/applyDefaults');
+
+const REGEX_NAME = /^[a-zA-Z0-9\-\/' ]*$/;
 
 function removeWhitespace(value) {
   return value.replace(/\s*/g, '');
@@ -14,14 +15,12 @@ function removeWhitespaceWithDelay(value, cb) {
   }, 500);
 }
 
-let filters = {};
 let schema = {};
-let defaults = {};
 
 describe('schema-validator', () => {
 
   beforeEach(() => {
-    filters = {};
+
     schema = {};
   });
 
@@ -30,18 +29,23 @@ describe('schema-validator', () => {
     it('should return true when valid', (done) => {
 
       schema = {
-        firstName: [[validate.required, 'Your name is required']]
-      };
-
-      filters = {
-        firstName: [removeWhitespace]
+        firstName: {
+          filters: [removeWhitespace],
+          validators: [
+            [validate.maxlength(24), 'Sorry, our system can only handle up to 24 characters.'],
+            [validate.regex(REGEX_NAME), 'First name contains invalid characters']
+          ],
+          empty: {
+            default: ''
+          }
+        }
       };
 
       const objectToValidate = {
         firstName: 'Im here'
       };
 
-      validator.validate(defaults, filters, schema, objectToValidate).then((result) => {
+      validator.validate(schema, objectToValidate).then((result) => {
         assert(result.valid);
         done();
       }).catch((err) => {
@@ -53,20 +57,22 @@ describe('schema-validator', () => {
     it('should apply filter to specified value before validating', (done) => {
 
       schema = {
-        phoneNumber: [[validate.required, 'Your phoneNumber is required'],
-                      [validate.maxlength(10), 'Your phone number is too long']]
-      };
- 
-      filters = {
-        phoneNumber: [removeWhitespace]
+        phoneNumber: {
+          filters: [removeWhitespace],
+          validators: [
+            [validate.maxlength(10), 'Your phone number is too long']
+          ],
+          empty: {
+            default: ''
+          }
+        }
       };
 
       const objectToValidate = {
-        firstName: 'I have no filter',
         phoneNumber: '02 9999 5555'
       };
 
-      validator.validate(defaults, filters, schema, objectToValidate).then((result) => {
+      validator.validate(schema, objectToValidate).then((result) => {
         assert(result.valid);
         assert.equal(result.values.phoneNumber, '0299995555');
         done();
@@ -79,20 +85,22 @@ describe('schema-validator', () => {
     it('should apply long running filter to specified value before validating', (done) => {
 
       schema = {
-        phoneNumber: [[validate.required, 'Your phoneNumber is required'],
-                      [validate.maxlength(10), 'Your phone number is too long']]
-      };
- 
-      filters = {
-        phoneNumber: [removeWhitespaceWithDelay]
+        phoneNumber: {
+          filters: [removeWhitespaceWithDelay],
+          validators: [
+            [validate.maxlength(10), 'Your phone number is too long']
+          ],
+          empty: {
+            default: ''
+          }
+        }
       };
 
       const objectToValidate = {
-        firstName: 'I have no filter',
         phoneNumber: '02 9999 5555'
       };
 
-      validator.validate(defaults, filters, schema, objectToValidate).then((result) => {
+      validator.validate(schema, objectToValidate).then((result) => {
         
         assert(result.valid);
         assert.equal(result.values.phoneNumber, '0299995555');
@@ -105,15 +113,24 @@ describe('schema-validator', () => {
 
     it('should apply filter to multiple values before validating', (done) => {
 
+
       schema = {
-        firstName: [[validate.maxlength(5), 'Your name is too long']],
-        phoneNumber: [[validate.required, 'Your phoneNumber is required'],
-                      [validate.maxlength(10), 'Your phone number is too long']]
-      };
- 
-      filters = {
-        firstName: [removeWhitespaceWithDelay],
-        phoneNumber: [removeWhitespaceWithDelay]
+        phoneNumber: {
+          filters: [removeWhitespaceWithDelay],
+          validators: [
+            [validate.maxlength(10), 'Your phone number is too long']
+          ],
+          empty: {
+            default: ''
+          }
+        },
+        firstName: {
+          filters: [removeWhitespaceWithDelay],
+          validators: [[validate.maxlength(5), 'Your name is too long']],
+          empty: {
+            default: ''
+          }
+        }
       };
 
       const objectToValidate = {
@@ -121,7 +138,7 @@ describe('schema-validator', () => {
         phoneNumber: '02 9999 5555'
       };
 
-      validator.validate(defaults, filters, schema, objectToValidate).then((result) => {
+      validator.validate(schema, objectToValidate).then((result) => {
         
         assert(result.valid);
         assert.equal(result.values.firstName, 'Mario');
@@ -133,26 +150,35 @@ describe('schema-validator', () => {
 
     }); 
 
-    it('should handle applying filter where object property has not been defined', (done) => {
+    it('should default value and filter where object property has not been defined', (done) => {
 
       schema = {
-        firstName: [[validate.maxlength(5), 'Your name is too long']],
-        phoneNumber: [[validate.required, 'Your phoneNumber is required'],
-                      [validate.maxlength(10), 'Your phone number is too long']]
+        phoneNumber: {
+          filters: [removeWhitespace],
+          validators: [
+            [validate.maxlength(10), 'Your phone number is too long']
+          ],
+          empty: {
+            default: ''
+          }
+        },
+        firstName: {
+          filters: [removeWhitespaceWithDelay],
+          validators: [[validate.maxlength(5), 'Your name is too long']],
+          empty: {
+            default: ''
+          }
+        }
       };
- 
-      filters = {
-        firstName: [removeWhitespaceWithDelay],
-        phoneNumber: [removeWhitespace]
-      };
-
+      
       const objectToValidate = {
         firstName: 'Mar io'  // phoneNumber has not yet been defined        
       };
 
-      validator.validate(defaults, filters, schema, objectToValidate).then((result) => {  
-        assert.equal(result.valid, false);
+      validator.validate(schema, objectToValidate).then((result) => {  
+        assert.equal(result.valid, true);
         assert.equal(result.values.firstName, 'Mario');
+        assert.equal(result.values.phoneNumber, '');
         done();
       }).catch((err) => {
         done(err);
@@ -163,11 +189,29 @@ describe('schema-validator', () => {
     it('should return true when valid for multiple properties on schema', (done) => {
 
       schema = {
-        firstName: [[validate.required, 'Your name is required']],
-        lastName: [[validate.required, 'Your name is required']],
-        middleName: [[validate.required, 'Your name is required']]
+        firstName: {
+          filters: [removeWhitespaceWithDelay],
+          validators: [[validate.maxlength(20), 'Your name is too long']],
+          empty: {
+            default: ''
+          }
+        },
+        lastName: {
+          filters: [removeWhitespaceWithDelay],
+          validators: [[validate.maxlength(20), 'Your name is too long']],
+          empty: {
+            default: ''
+          }
+        },
+        middleName: {
+          filters: [removeWhitespaceWithDelay],
+          validators: [[validate.maxlength(20), 'Your name is too long']],
+          empty: {
+            default: ''
+          }
+        }
       };
-
+      
       const objectToValidate = {
         firstName: 'Im here',
         lastName: 'And here',
@@ -176,7 +220,7 @@ describe('schema-validator', () => {
 
       assert(Object.keys(schema).length > 1);
 
-      validator.validate(defaults, filters, schema, objectToValidate).then((result) => {
+      validator.validate(schema, objectToValidate).then((result) => {
         assert.equal(result.valid, true);
         done();
       }).catch((err) => {
@@ -185,12 +229,30 @@ describe('schema-validator', () => {
 
     });
 
-    it('should return false when one property is in-valid', (done) => {
+    it('should return false when one property is invalid', (done) => {
 
       schema = {
-        firstName: [[validate.required, 'Your name is required']],
-        lastName: [[validate.required, 'Your name is required']],
-        middleName: [[validate.required, 'Your name is required']]
+        firstName: {
+          filters: [removeWhitespaceWithDelay],
+          validators: [[validate.maxlength(20), 'Your name is too long']],
+          empty: {
+            default: ''
+          }
+        },
+        lastName: {
+          filters: [removeWhitespaceWithDelay],
+          validators: [[validate.maxlength(20), 'Your name is too long']],
+          empty: {
+            default: ''
+          }
+        },
+        middleName: {
+          filters: [removeWhitespaceWithDelay],
+          validators: [[validate.maxlength(4), 'Your name is too long']],
+          empty: {
+            default: ''
+          }
+        }
       };
 
       const objectToValidate = {
@@ -199,7 +261,7 @@ describe('schema-validator', () => {
         middleName: 'But not here'
       };
 
-      validator.validate(defaults, filters, schema, objectToValidate).then((result) => {
+      validator.validate(schema, objectToValidate).then((result) => {
         assert.equal(result.valid, false);
         done();
       }).catch((err) => {
@@ -208,23 +270,41 @@ describe('schema-validator', () => {
 
     });
 
-    it('should return false when all properties are in-valid', (done) => {
+    it('should return false when all properties are invalid', (done) => {
 
       schema = {
-        firstName: [[validate.required, 'Your name is required']],
-        lastName: [[validate.required, 'Your name is required']],
-        middleName: [[validate.required, 'Your name is required']]
+        firstName: {
+          filters: [removeWhitespaceWithDelay],
+          validators: [[validate.maxlength(4), 'Your name is too long']],
+          empty: {
+            default: ''
+          }
+        },
+        lastName: {
+          filters: [removeWhitespaceWithDelay],
+          validators: [[validate.maxlength(4), 'Your name is too long']],
+          empty: {
+            default: ''
+          }
+        },
+        middleName: {
+          filters: [removeWhitespaceWithDelay],
+          validators: [[validate.maxlength(4), 'Your name is too long']],
+          empty: {
+            default: ''
+          }
+        }
       };
 
       const objectToValidate = {
-        firstName: '',
-        lastName: '',
-        middleName: ''
+        firstName: 'Im here',
+        lastName: 'arghhhhhh',
+        middleName: 'But not here'
       };
 
       assert(Object.keys(schema).length > 1);
 
-      validator.validate(defaults, filters, schema, objectToValidate).then((result) => {
+      validator.validate(schema, objectToValidate).then((result) => {
         assert.equal(result.valid, false);
         done();
       }).catch((err) => {
@@ -236,17 +316,23 @@ describe('schema-validator', () => {
     it('should return false when one out of many schema rules is invalid for single property', (done) => {
 
       schema = {
-        firstName: [
-          [validate.required, 'Your name is required'],
-          [validate.minlength(8), 'Name must be min 8 characters']
-        ]
+        firstName: {
+          filters: [removeWhitespace],
+          validators: [
+            [validate.maxlength(24), 'Sorry, our system can only handle up to 24 characters.'],
+            [validate.regex(REGEX_NAME), 'First name contains invalid characters']
+          ],
+          empty: {
+            default: ''
+          }
+        }
       };
 
       const objectToValidate = {
-        firstName: 'Apu'
+        firstName: 'Im here$'
       };
 
-      validator.validate(defaults, filters, schema, objectToValidate).then((result) => {
+      validator.validate(schema, objectToValidate).then((result) => {
         assert.equal(result.valid, false);
         done();
       }).catch((err) => {
@@ -258,17 +344,23 @@ describe('schema-validator', () => {
     it('should return true when all schema rules are valid for single property', (done) => {
 
       schema = {
-        firstName: [
-          [validate.required, 'Your name is required'],
-          [validate.minlength(8), 'Name must be min 8 characters']
-        ]
+        firstName: {
+          filters: [removeWhitespace],
+          validators: [
+            [validate.minlength(8), 'Sorry, our system.'],
+            [validate.regex(REGEX_NAME), 'First name contains invalid characters']
+          ],
+          empty: {
+            default: ''
+          }
+        }
       };
 
       const objectToValidate = {
         firstName: 'MoreThanEightChars'
       };
 
-      validator.validate(defaults, filters, schema, objectToValidate).then((result) => {
+      validator.validate(schema, objectToValidate).then((result) => {
         assert.equal(result.valid, true);
         done();
       }).catch((err) => {
@@ -277,75 +369,34 @@ describe('schema-validator', () => {
 
     });
 
-    it('should return values of valid fields', () => {
+    it('should return values of valid fields only', () => {
 
       schema = {
-        firstName: [[validate.required, 'Your first name is required']],
-        lastName: [[validate.required, 'Your last name is required']]
+        firstName: {
+          filters: [removeWhitespace],
+          validators: [
+            [validate.maxlength(24), 'Sorry, our system.'],
+            [validate.regex(REGEX_NAME), 'First name contains invalid characters']
+          ],
+          empty: {
+            default: ''
+          }
+        },
+        lastName: {
+          filters: [removeWhitespace],
+          validators: [
+            [validate.maxlength(2), 'Sorry, our system hates you.'],
+            [validate.regex(REGEX_NAME), 'Last name contains invalid characters']
+          ],
+          empty: {
+            default: ''
+          }
+        }
       };
 
-      return validator.validate(defaults, filters, schema, {firstName: 'Matt'})
+      return validator.validate(schema, {firstName: 'Matt', lastName: 'Turner'})
         .then(result => assert.deepEqual(result.values, {firstName: 'Matt'}))
         ;
-    });
-    
-    it('should add defaulted field to result values', (done) => {
-      schema = {
-        firstName: [[validate.required, 'Your first name is required']],
-        defaultedProp: [[() => true, 'Straight through please.']]
-      };
-      
-      defaults = {
-        defaultedProp: 'default'
-      };
-
-      return validator.validate(defaults, filters, schema, {firstName: 'Matt'})
-        .then(result => {
-          assert.equal(result.values.defaultedProp, 'default');
-          done();
-        }).catch(err => done(err));
-    });
-  });
-  
-  describe('applyDefaults()', () => {
-    it('should apply defaults to values when not found on schema', (done) => {
-      
-      const defaultsSchema = {
-        firstName: '',
-        phoneNumber: '',
-        scale: 'Single'
-      };
-      
-      const values = {
-        firstName: 'Bob',
-        phoneNumber: '0423234530'
-      };
-      
-      defaulter(defaultsSchema, values).then(defaultedValues => {
-        assert.equal(defaultedValues.scale, defaultsSchema.scale);
-        done();
-      });
-      
-    });
-    
-    it('should do nothing to already set property when property has a default value specified', (done) => {
-      
-      const defaultsSchema = {
-        firstName: 'John',
-        phoneNumber: '',
-        scale: 'Single'
-      };
-      
-      const values = {
-        firstName: 'Bob',
-        phoneNumber: '0423234530'
-      };
-      
-      defaulter(defaultsSchema, values).then(defaultedValues => {
-        assert.equal(defaultedValues.firstName, values.firstName);
-        done();
-      });
-      
     });
     
   });
